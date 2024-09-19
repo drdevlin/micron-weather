e7d2387de66a0bd857deb8b41b820b9e6cad06d4
drdevlin <drdevlin@fastmail.com>
2024-09-19 15:53:27 -0400

    feat: add Search, Report to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Search } from '@/components/Search';
+import { Report } from '@/components/Report';
+
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <Search />
+      <Report />
     </main>
   );
 }
