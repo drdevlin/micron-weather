4e37d9bfe56afaaf3ac91f620d4aa6f7fe127c66
drdevlin <drdevlin@fastmail.com>
2024-09-20 11:18:13 -0400

    feat: update home page with fetch weather action

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
+'use client'
+
+import { useFormState } from 'react-dom';
+import { getReport } from '@/app/actions';
 import { Search } from '@/components/Search';
 import { Report } from '@/components/Report';
 
+import type { ReportState } from '@/app/actions';
+
 import styles from "./page.module.css";
 
+const initialState: ReportState = [null, null];
+
 export default function Home() {
+  const [state, action] = useFormState(getReport, initialState);
+
   return (
     <main className={styles.main}>
-      <Search />
-      <Report />
+      <Search action={action} />
+      <Report state={state} />
     </main>
   );
 }
