ce50fbc84a1bbdc25ae23931e90b7461080b209b
drdevlin <drdevlin@fastmail.com>
2024-09-20 11:15:13 -0400

    feat: build out dynamic Report component

--- components/Report.tsx
+++ components/Report.tsx
@@ -1,9 +1,22 @@
+import type { ReportState } from '@/app/actions';
+
 import styles from './Report.module.css';
 
-export const Report = () => {
+/** REPORT
+ * Displays the current temperature.
+ */
+export interface ReportProps {
+  // State containing the temperature
+  // or error message.
+  state: ReportState;
+}
+export const Report = ({ state }: ReportProps) => {
+  const [error, temperature] = state;
+
   return (
-    <section className={styles.report}>
-      <p>23°</p>
+    <section className={styles.report} aria-live="polite">
+      {error && <p className={styles.error}>{error}</p>}
+      {temperature && <p className={styles.temperature}>{temperature + '°'}</p>}
     </section>
   );
 };
