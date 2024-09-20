2c16beaf77bc246bfea20b174631e09a60f06358
drdevlin <drdevlin@fastmail.com>
2024-09-20 11:16:44 -0400

    feat: make Search component work

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,19 +1,34 @@
+import { Submit } from './Submit';
+
 import styles from './Search.module.css';
 
-export const Search = () => {
+/** SEARCH
+ * Contains the form for finding the current temperature.
+ */
+export interface SearchProps {
+  // The React server action for the form.
+  // It should fetch the current temperature
+  // given a `city`.
+  action: (payload: FormData) => void;
+}
+export const Search = ({ action }: SearchProps) => {
   return (
     <section className={styles.search}>
 
-      <form>
-        <label>Current Temperature at</label>
+      <form action={action}>
+        <label>CURRENT TEMPERATURE AT:</label>
 
         <div className={styles.searchBar}>
-          <input type="search" placeholder="Enter City"/>
-          <button>
-            <svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
-          </button>
+          <input
+            type="search"
+            name="city"
+            placeholder="Enter City"
+            title="City [State Code, Country Code]"
+            aria-label="city"
+          />
+          <Submit />
         </div>
-        
+
       </form>
 
     </section>
