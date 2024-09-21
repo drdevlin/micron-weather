48fc445b74b83b4ac4e525499aa50383b2e64beb
drdevlin <drdevlin@fastmail.com>
2024-09-21 10:10:47 -0400

    feat: add actions for storing, retrieving preferred city

--- app/actions.ts
+++ app/actions.ts
@@ -1,5 +1,8 @@
 'use server'
 
+import { cookies } from 'next/headers';
+import { supabase } from '@/app/db';
+
 const appid = process.env.WEATHER_SERVICE_API_KEY;
 const limit = '1';
 const units = 'metric';
@@ -8,9 +11,13 @@ const apiKeyParam = '&appid=' + appid;
 const limitParam = '&limit=' + limit;
 const unitsParam = '&units=' + units;
 
+type Weather = {
+  city: string;
+  temperature: string;
+}
 type ReportNull = [null, null]
 type ReportError = [string, null];
-type ReportReceived = [null, string];
+type ReportReceived = [null, Weather];
 export type ReportState = ReportNull | ReportError | ReportReceived;
 
 /* Retreives the current temperature for a given city */
@@ -20,6 +27,7 @@ export const getReport = async (prevState: ReportState, formData: FormData): Pro
     // Get city name
     const query = formData.get('city');
     if (!query) return [null, null];
+    if (typeof query !== 'string') return [null, null];
   
     // Assemble search params
     const geoSearchParams = `?q=${query}` + limitParam + apiKeyParam;
@@ -45,9 +53,52 @@ export const getReport = async (prevState: ReportState, formData: FormData): Pro
     const weather = await weatherResponse.json();
     const temperature = String(Math.round(weather.main.temp));
   
-    return [null, temperature];
+    return [
+      null,
+      {
+        city: query,
+        temperature,
+      },
+    ];
   } catch(error) {
     console.error(error);
     return ['Something went wrong.', null];
   }
 }
+
+/* Stores city in the database with an associated fingerprint. */
+export const bookmarkCity = async (city: string) => {
+  // Try to get the fingerprint.
+  const fingerprint = cookies().get('fingerprint');
+
+  // If there isn't one, we want to insert a new row.
+  const insert = async () => supabase.from('users').insert({ preferred_city: city }).select('fingerprint');
+  // If there is one, we want to update it.
+  const update = async () => supabase.from('users').update({ preferred_city: city }).eq('fingerprint', fingerprint?.value).select('fingerprint');
+
+  // Decide.
+  const { data, error } = fingerprint ? await update() : await insert();
+
+  // On Error.
+  error && console.error(error);
+  if (!data) return false;
+
+  // On Success.
+  const [user] = data;
+  cookies().set('fingerprint', user.fingerprint);
+  return true;
+};
+
+/* Retrieves the user's preferred city from the database. */
+export const getPreferredCity = async () => {
+  const fingerprint = cookies().get('fingerprint');
+  if (!fingerprint) return;
+
+  const { data, error } = await supabase.from('users').select('preferred_city').eq('fingerprint', fingerprint?.value);
+
+  error && console.error(error);
+  if (!data) return;
+
+  const [user] = data;
+  return user.preferred_city;
+};
