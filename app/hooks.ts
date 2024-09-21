b0ec10b90e2cd349484c6d22bc5fb43d4a1eebf1
drdevlin <drdevlin@fastmail.com>
2024-09-21 10:12:03 -0400

    feat: add hook for loading preferred city

---
+++ app/hooks.ts
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+import { getPreferredCity } from '@/app/actions';
+
+/* Fetches the user's preferred city from the database by calling the `getPreferredCity` action */
+/* Then fetches the weather by calling `action` */
+export const usePreferredCity = (action: (payload: FormData) => void) => {
+  useEffect(() => {
+    (async () => {
+      // Retrieve from db.
+      const preferredCity = await getPreferredCity();
+      if (!preferredCity) return;
+      
+      // Submit form.
+      const formData = new FormData;
+      formData.set('city', preferredCity);
+      action(formData);
+    })();
+  }, []);
+};
